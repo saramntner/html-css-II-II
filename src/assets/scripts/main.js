d8d85ee69ad50043fd9450f2f77bd1d017ce63b6
Sara Montaner Perea <smontaner0@uoc.edu>
2025-05-05 16:14:16 +0200

    HTML semántico, CSS para estilos, imágenes y dependencias

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -6,11 +6,8 @@
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
+import * as bootstrap from 'bootstrap';
+
 /**
  * Write any other JavaScript below
  */
-
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
